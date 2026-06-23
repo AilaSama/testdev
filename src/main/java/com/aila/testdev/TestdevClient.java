d8d5a9e6f20797db750f0495acd756985fffb389
Aila_Sama <pubg1040813717@gmail.com>
2026-06-23 13:31:42 +0800

    Initial commit

---
+++ src/main/java/com/aila/testdev/TestdevClient.java
@@ -0,0 +1,10 @@
+package com.aila.testdev;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestdevClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/aila/testdev/TestdevDataGenerator.java
@@ -0,0 +1,11 @@
+package com.aila.testdev;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TestdevDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
